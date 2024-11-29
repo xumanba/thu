49acf05198a7c75b3ce50d1c3a8e8d285960177c
xumanba <122879066+xumanba@users.noreply.github.com>
2024-11-29 15:00:16 +0800

    Add files via upload

---
+++ download_monet.sh
@@ -0,0 +1,11 @@
+FILE=$1
+
+echo "Specified [$FILE]"
+
+mkdir -p ./checkpoints/${FILE}_pretrained
+
+MODEL_FILE=./checkpoints/${FILE}_pretrained/gen_monet.pth
+
+URL=https://cloud.tsinghua.edu.cn/f/f85c4ce0a4a3471fb6c6/?dl=1
+
+wget -N $URL -O $MODEL_FILE
