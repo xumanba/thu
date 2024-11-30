fd1f53d44326380daa568e412c1d6c8fd94b8ed7
xumanba <122879066+xumanba@users.noreply.github.com>
2024-11-30 13:27:19 +0800

    Update download_monet.sh

--- download_monet.sh
+++ download_monet.sh
@@ -1,6 +1,6 @@
 FILE=$1
 echo "Specified [$FILE]"
 mkdir -p ./checkpoints/${FILE}_pretrained
-MODEL_FILE=./checkpoints/${FILE}_pretrained/gen_monet.pth
+MODEL_FILE=./checkpoints/${FILE}_pretrained/
 URL=https://cloud.tsinghua.edu.cn/f/423ed51e61d04235b76f/?dl=1
 wget -N $URL -O $MODEL_FILE
