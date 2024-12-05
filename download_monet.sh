cda1bcbcac45edcc3c6298424425cb2d0eacd0cd
xumanba <122879066+xumanba@users.noreply.github.com>
2024-12-05 14:00:11 +0800

    Update download_monet.sh

--- download_monet.sh
+++ download_monet.sh
@@ -1,4 +1,4 @@
 FILE=$1
 echo "Specified [$FILE]"
-URL=https://cloud.tsinghua.edu.cn/seafhttp/files/dcbed4a3-a81c-4d3c-8dbf-959cde712f64/gen_monet_epoch_18.pth
+URL=https://cloud.tsinghua.edu.cn/seafhttp/files/db134882-39a2-4fa2-a7cd-c9a9f3ad3680/gen_monet20_identity.pth
 wget $URL
